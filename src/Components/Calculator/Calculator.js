7c4578a5981a1e64e2a82179a2fa7e0173cfcb10
sinaafshar13 <afshari.sina@outlook.com>
2024-02-29 13:59:52 +0330

    create calculator-mini-project

---
+++ src/Components/Calculator/Calculator.js
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import "./Calculator.css";
+import { evaluate } from "mathjs";
+function Calculator() {
+  let [result, setResult] = useState("");
+  let [hasDot, setHasDot] = useState(false);
+
+  const operators = ["+", "-", "*", "/"];
+  const checkInput = (text) => {
+    if (text === "×") return "*";
+    if (text === "÷") return "/";
+    return text;
+  };
+  const clickHandler = (e) => {
+    let input = checkInput(e.target.innerText);
+    if (input === ".") {
+      if (hasDot === true) return;
+      else setHasDot(true);
+    }
+    if (operators.includes(input)) setHasDot(false);
+    setResult(result + input);
+  };
+  const clearBtn = () => {
+    setResult("");
+    setHasDot(false);
+  };
+  const cBtn = () => {
+    if (result.endsWith(".")) setHasDot(false);
+    setResult(result.slice(0, -1));
+  };
+  const equlBtn = () => {
+    setResult(evaluate(result).toString());
+    setHasDot(false);
+  };
+  return (
+    <div className="container">
+      <div className="screen">{result}</div>
+      <div className="buttons">
+        <button onClick={clearBtn} className="color twoCol">
+          Clear
+        </button>
+        <button onClick={cBtn} className="color">
+          C
+        </button>
+        <button onClick={clickHandler} className="color">
+          ÷
+        </button>
+        <button onClick={clickHandler}>7</button>
+        <button onClick={clickHandler}>8</button>
+        <button onClick={clickHandler}>9</button>
+        <button onClick={clickHandler} className="color">
+          ×
+        </button>
+        <button onClick={clickHandler}>4</button>
+        <button onClick={clickHandler}>5</button>
+        <button onClick={clickHandler}>6</button>
+        <button onClick={clickHandler} className="color">
+          -
+        </button>
+        <button onClick={clickHandler}>1</button>
+        <button onClick={clickHandler}>2</button>
+        <button onClick={clickHandler}>3</button>
+        <button onClick={clickHandler} className="color">
+          +
+        </button>
+        <button onClick={clickHandler}>0</button>
+        <button onClick={clickHandler}>.</button>
+        <button onClick={equlBtn} className="color twoCol">
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Calculator;
